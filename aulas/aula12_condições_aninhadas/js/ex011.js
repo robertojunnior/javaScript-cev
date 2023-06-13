3e2e7e3b8f8bbf045afa0e519753d98b6d3bab43
roberjunnior <roberjunior.dev@gmail.com>
2023-06-13 20:38:39 -0300

    update - ex011 - Else If

--- aulas/aula12_condições_aninhadas/js/ex011.js
+++ aulas/aula12_condições_aninhadas/js/ex011.js
@@ -1,12 +1,13 @@
-var idade = 65
-console.log(`Você tem ${idade} anos.`)
-if (idade < 16) {
-    console.log('Você ainda não pode votar.')
-} else if (idade < 18 || idade >= 65) {
-    console.log(`Com ${idade} anos, o voto é opcional.`) 
-} else {
-    console.log('Você atingiu a maioridade, seu voto é obrigatório.')
+function validar() {
+    var digiteIdade = document.querySelector('input#digiteIdade')
+    var idade = Number(digiteIdade.value)
+    var resultado = document.querySelector('div.resultado') 
+
+    if (idade < 16) {
+        resultado.innerHTML = `Você ainda não pode votar.`
+    } else if (idade < 18 || idade >= 65) {
+        resultado.innerHTML = `Com ${idade} anos, o voto é opcional.`
+    } else {
+        resultado.innerHTML = `Você atingiu a maioridade, seu voto é obrigatório.`
+    }
 }
-
-
-
