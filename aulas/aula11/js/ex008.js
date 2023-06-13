ad66871836425f7c9202a726a7bbb17ec9aeeccd
roberjunnior <roberjunior.dev@gmail.com>
2023-06-13 01:20:16 -0300

    update aula11

---
+++ aulas/aula11/js/ex008.js
@@ -0,0 +1,8 @@
+var velocidade = 60.1
+console.log(`A velocidade aferida do seu carro foi de ${velocidade}km/h.`)
+
+if(velocidade > 60) { //condição simples
+    console.log(`Você ultrapassou a velocidade permitida para esta via. MULTADO!`)
+}
+
+console.log(`Dirija sempre com cinto de segurança.`)
---
+++ aulas/aula11/js/ex009.js
@@ -0,0 +1,8 @@
+var país = 'Mauá'
+console.log(`Se o seu país é ${país},`)
+
+if(país != 'Brasil') {
+    console.log('você é Estrangeiro.')
+} else {
+    console.log('você é um Brasileiro.')
+}
