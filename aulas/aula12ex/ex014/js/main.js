515657db6929d72ff7af8818998f83fbc82bb550
roberjunnior <roberjunior.dev@gmail.com>
2023-06-24 01:57:44 -0300

    update ex014

--- aulas/aula12ex/ex014/js/main.js
+++ aulas/aula12ex/ex014/js/main.js
@@ -1,7 +1,16 @@
 function carregar() {
   let mensagem = document.getElementById("mensagem");
-  let fotoHorario = document.getElementById("imagemDia");
+  var foto = document.getElementById("imagem");
   let data = new Date()
   var hora = data.getHours()
   mensagem.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12){
+        foto.src = 'imagem/amanhecer.jpg'
+    } else if (hora > 12 && hora < 18) {
+        foto.src = 'imagem/entardecer.jpg'
+    } else {
+        foto.src = 'imagem/anoitecer.jpg'
+    }
+
 }
