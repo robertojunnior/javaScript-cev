9d2e6cc55a127e5e81e6c82a7f756ef704d5e367
roberjunnior <roberjunior.dev@gmail.com>
2023-06-26 02:11:46 -0300

    update - Final exercicios

--- aulas/aula12ex/ex015/js/main.js
+++ aulas/aula12ex/ex015/js/main.js
@@ -14,23 +14,55 @@ function verificar() { // função criada lá no html
         
         // var genero começa vazia pois vai ser escolhida no radio checked
         var genero = ''
+        // var imagem cria uma tag no html de img com createElement
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto') // criou para a tag img um 'id' com nome ' foto'.
+        // se o sexo escolhido for na posição [0] é masculino então genero igual homem
         if(sexoEscolhido[0].checked){
-            genero = 'Homem'
+            genero = 'homem'
+            if (idade >= 0 && idade < 3){
+                // bebe
+                img.setAttribute('src', 'imagem/bebe.png') // este atributo 'src' é o source
+            } else if (idade >= 4 && idade <= 10) {
+                //criança
+                img.setAttribute('src', 'imagem/menino.png')
+            } else if (idade > 11 && idade < 17){
+                // jovem
+                img.setAttribute('src', 'imagem/jovem-homem.png')
+            } else if (idade >= 18 && idade <= 59) {
+                //adulto
+                img.setAttribute('src', 'imagem/homem.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'imagem/senhor.png')
+            }
         } else {
-            genero = 'Mulher'
+            genero = 'mulher'
+            if (idade >= 0 && idade < 3){
+                // bebe
+                img.setAttribute('src', 'imagem/bebe.png')
+            } else if (idade >= 4 && idade <= 13) {
+                //criança
+                img.setAttribute('src', 'imagem/menina.png')
+            } else if (idade > 14 && idade < 17){
+                // jovem
+                img.setAttribute('src', 'imagem/jovem-mulher.png')
+            } else if (idade >= 18 && idade <= 59) {
+                //adulto
+                img.setAttribute('src', 'imagem/mulher.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'imagem/senhora.png')
+            }
         }
 
-
-
-
-
-
-
-
-
-
-
-        
         saida.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        saida.appendChild(img) 
+        // o appendChild demonstrara o conteudo do 'id' img criado anteriormente
     }
 }
+
+// if(idade <= 10) img.setAttribute('src', `images/${genero}/baby.png`)
+//         else if(idade <= 21) img.setAttribute('src', `images/${genero}/teen.png`)
+//         else if(idade <= 50) img.setAttribute('src', `images/${genero}/adult.png`)
+//         else img.setAttribute('src', `images/${genero}/old.png`)
