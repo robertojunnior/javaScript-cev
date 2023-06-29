a88f360df0c88814db0630cc71a427f4317b44e5
roberjunnior <roberjunior.dev@gmail.com>
2023-06-28 22:29:28 -0300

    update aula 14 ex loop

---
+++ aulas/aula14ex_repeticoes/ambiente.js
@@ -0,0 +1,15 @@
+// while e do while
+// var c = 1
+// while (c <= 10){
+//     console.log(c)
+//     c++
+// }
+
+//-----------------------------
+// for
+// variavel ou inicio ; teste logico ; incremento
+for (var c = 1; c <= 10; c++){
+    console.log(c)
+}
+
+
