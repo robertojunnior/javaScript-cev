6114395de83e48b30a23cb53a2f69f44355120ba
roberjunnior <roberjunior.dev@gmail.com>
2023-06-28 21:36:29 -0300

    update - ambiente while

---
+++ aulas/aula13/node_721fbfb33b25d.tmp.js
@@ -0,0 +1,21 @@
+// console.log('Tudo Bem')
+// console.log('Tudo Bem')
+// console.log('Tudo Bem')
+// console.log('Tudo Bem')
+// console.log('Tudo Bem')
+// console.log('Tudo Bem')
+
+/*
+let contador = 1
+
+while (contador <= 6){ // enauqnto for menor dou igual a 6 
+    console.log('Tudo Bem', contador) // faça isso
+    contador ++ // contador = contador + 1
+}
+*/
+
+let carneirinhos = 1
+
+do {
+    console.log(carneirinhos, 'Carneirinhos')
+} while ( carneirinhos < 7)
