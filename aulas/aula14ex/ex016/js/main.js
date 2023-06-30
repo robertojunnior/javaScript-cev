4f74a6d88738352e18c1825d3f975d6d26b06993
roberjunnior <roberjunior.dev@gmail.com>
2023-06-30 04:31:04 -0300

    update

--- aulas/aula14ex/ex016/js/main.js
+++ aulas/aula14ex/ex016/js/main.js
@@ -1,11 +1,34 @@
 function contar() {
-    let inicio = document.getElementById('textoInicio')
-    let fim = document.getElementById('textoFim')
-    let passo = document.getElementById('textoPasso')
+  let inicio = document.getElementById("textoInicio");
+  let fim = document.getElementById("textoFim");
+  let passo = document.getElementById("textoPasso");
+  let resultado = document.getElementById("resultado");
 
-    if (inicio.value === 0 || fim.value === 0 || passo || 0) {
-        alert('[ERRO] Faltam dados para executar!')
-    } else {
-        alert('Tudo ok!')
+  if (inicio.value === 0 || fim.value === 0 || passo.value === 0) {
+    // alert("[ERRO] Faltam dados para executar!");
+    resultado.innerHTML = "Insira os dados novamente!:";
+  } else {
+    resultado.innerHTML = "Contando: <br>"
+    let i = Number(inicio.value);
+    let f = Number(fim.value);
+    let p = Number(passo.value);
+
+    if (p <= 0) {
+        alert('Passo inválido! Cosiderando PASSO = 1') 
+        p = 1
     }
+
+    if (i < f) {
+        // contador crescente
+        for (let contador = i; contador <= f; contador += p) {
+            resultado.innerHTML += ` ${contador} \u{1F449}`;
+        }
+    } else {
+        //contador decrescente
+        for (let contador = i; contador >= f; contador -= p){
+            resultado.innerHTML += ` ${contador} \u{1F449}`;
+        }
+    }
+    resultado.innerHTML += `Fim do Contador \u{1F3C1}`;
+  }
 }
