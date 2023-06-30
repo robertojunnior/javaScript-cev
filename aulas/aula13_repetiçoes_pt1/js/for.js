d5098f8bf9156f9700324af9641a9614684a3941
roberjunnior <roberjunior.dev@gmail.com>
2023-06-29 23:59:15 -0300

    update

--- aulas/aula13_repetiçoes_pt1/js/for.js
+++ aulas/aula13_repetiçoes_pt1/js/for.js
@@ -18,7 +18,3 @@ for (var c = 1; c <= 4; c++){
     console.log(c)
 }
 console.log('Terminou.')
-
-
-
-
---
+++ aulas/aula14_repeticoes_pt2/ambiente.js
@@ -0,0 +1,20 @@
+// while e do while
+// var c = 1                   (recebe o valor 1)
+// while (c <= 10){            (enquanto c for menor ou igual a 10)
+//     console.log(c)          (imprima avariavel c)
+//     c++                     (e some mais um até que o valor seja 10)
+// }
+
+//-----------------------------
+// for
+// variavel ou inicio ; teste logico ; incremento
+// for (var c = 1; c <= 10; c++){
+//     console.log(c)
+// }
+
+//------------------------------
+console.log('Vai começar...')
+for (var c = 1; c <= 4; c++){
+    console.log(c)
+}
+console.log('Terminou.')
