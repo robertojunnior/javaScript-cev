375fdbe53e033ed5085a3d12d6883827b58bb784
roberjunnior <roberjunior.dev@gmail.com>
2023-07-04 09:17:58 -0300

    update

--- aulas/aula16_funções/js/funcao04.js
+++ aulas/aula16_funções/js/funcao04.js
@@ -1,4 +1,4 @@
-function fatoria(numero) {
+function fatorial(numero) {
     let fat = 1
     for (let contador = numero; contador > 1; contador-- ) {
         fat *= contador // fat = fat * contador
@@ -6,7 +6,7 @@ function fatoria(numero) {
     return fat
 }
 //imprima na tela o fatorial de 5
-console.log (fatoria(5))
+console.log (fatorial(5))
 
 // fatorial de 5
 // 5! = 5 x 4 x 3 x 2 x 1 = 120
