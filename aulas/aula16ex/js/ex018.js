b764624b34598c9d5d607d3d5ce162d389add0ab
roberjunnior <roberjunior.dev@gmail.com>
2023-07-04 00:40:26 -0300

    update ex018

---
+++ aulas/aula16ex/js/ex018.js
@@ -0,0 +1,22 @@
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function  isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, 1)
+
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+
+    } else {
+        window.alert('Valor não encontrado ou já existente!')
+    }
+}
